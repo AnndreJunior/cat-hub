cae9155e047b8fb950e1f5add8929b0b9e705500
AnndreJunior <anndre.junior15@gmail.com>
2024-12-12 17:47:14 -0300

    Revert "refactor: create a module to render the header"
    This reverts commit 55bc52040abc9a01b6e82dd30d24dfc0db9fb9fc.

--- js/about.js
+++ js/about.js
@@ -1,3 +1,38 @@
-import { configureNavigation } from "./modules/navigation.js";
+import { createHeaderElements } from "./modules/create-header-elements.js";
+import { isAuthenticated } from "./modules/is-authenticated.js";
 
-configureNavigation();
+const { headerSection, ul, loginLi, registerLi, nav, loginLink, registerLink } =
+  createHeaderElements();
+
+if (
+  isAuthenticated() == "false" ||
+  isAuthenticated() === null ||
+  isAuthenticated() === undefined
+) {
+  loginLink.setAttribute("href", "../../pages/login");
+  loginLink.textContent = "Entrar";
+  registerLink.setAttribute("href", "../../pages/register");
+  registerLink.textContent = "Cadastrar";
+
+  loginLi.appendChild(loginLink);
+  registerLi.appendChild(registerLink);
+  ul.appendChild(loginLi);
+  ul.appendChild(registerLi);
+
+  nav.appendChild(ul);
+
+  headerSection.appendChild(nav);
+} else {
+  const userAvatarDiv = document.createElement("div");
+  const userAvatarImg = document.createElement("img");
+  userAvatarImg.setAttribute(
+    "src",
+    "../../assets/img/static/post-owner-avatar-image-example.png"
+  );
+  const userAvatarLink = document.createElement("a");
+  userAvatarLink.setAttribute("href", "../../pages/user?userId=1");
+
+  userAvatarLink.appendChild(userAvatarImg);
+  userAvatarDiv.appendChild(userAvatarLink);
+  headerSection.appendChild(userAvatarDiv);
+}
